22f619ec6f8c9a8b6e295d07ddfbe76c70596d69
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2025-03-14 11:20:07 +0100

    pb netlify 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
     output: 'server',
     adapter: node(),
     site: 'https://sae203.melvyn-philippon.fr',
     integrations: [
+        tailwind(),
         // Ajoutez ici d'autres intégrations si nécessaire
     ],
     build: {
