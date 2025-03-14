eeb14286142e88d25575fb625f9911c532200ae6
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2025-03-14 12:31:00 +0100

    Dernier Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    mode: 'ssr', // ou 'spa' selon vos besoins
+    mode: 'ssr',
     output: 'server',
     adapter: node(),
     site: 'https://sae203.melvyn-philippon.fr',
     integrations: [
         tailwind(),
-        // Ajoutez ici d'autres intégrations si nécessaire
     ],
     build: {
-        // Autres configurations de build si nécessaire
     },
 });
