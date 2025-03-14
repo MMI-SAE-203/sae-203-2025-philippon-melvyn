ea830b43c5d9224f0e31039b840115277f156a48
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2025-03-14 12:16:46 +0100

    Commit netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    mode: 'ssr', // ou 'spa' selon vos besoins
     output: 'server',
     adapter: node(),
     site: 'https://sae203.melvyn-philippon.fr',
     integrations: [
         tailwind(),
+        // Ajoutez ici d'autres intégrations si nécessaire
     ],
-    build: {e
+    build: {
+        // Autres configurations de build si nécessaire
     },
 });
